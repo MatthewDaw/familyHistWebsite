27173973bd5c1ac77df4f5982bb211494a6101b3
Matthew <mattdaw7@gmail.com>
2020-04-29 01:54:56 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+
+      <div id="appBar">
+
+        <div id="logo"></div>
+
+        <div class="rightSide">
+
+          <!-- <div class="userBox" id="myBids" >Your Bids</div> -->
+
+          <!-- <v-btn class="userBox" id="signUp">Sign Up</v-btn> -->
+
+          <v-btn  tile color="indigo" dark>Your Bids</v-btn>
+
+          <v-btn class="mx-2" fab dark color="red">
+            <span id="userButton">MD</span>
+          </v-btn>
+
+        </div>
+
+      </div>
+
+</v-app>
+</template>
+
+<style>
+  @import './assets/appbar.css';
+</style>
+
+<script>
+
+export default {
+  name: "App",
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+Vue.use(IconsPlugin)
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
